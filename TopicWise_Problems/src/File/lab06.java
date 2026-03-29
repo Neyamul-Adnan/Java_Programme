a6bd07552d16cae2c0b8b434df36710e8688d700
Neyamul Adnan <muhammadmojiburrahaman@gmail.com>
2026-03-29 21:06:39 +0600

    final

--- TopicWise_Problems/src/File/lab06.java
+++ TopicWise_Problems/src/File/lab06.java
@@ -13,23 +13,27 @@ public class lab06 {
 
 	public static void main(String[] args) throws Exception {
 		File in = new File("src/File/Input.txt");
-		FileWriter e = new FileWriter("src/File/Even.txt");
-		FileWriter o = new FileWriter("src/File/Odd.txt");
+		File e = new File("src/File/Even.txt");
+		File o = new File("src/File/Odd.txt");
+		
+		PrintWriter even = new PrintWriter(e);
+		PrintWriter odd = new PrintWriter(o);
 
 		Scanner sc = new Scanner(in);
 
 		while (sc.hasNextInt()) {
 			int num = sc.nextInt();
+			
 			if (num % 2 == 0) {
-				e.write(num + " ");
+				even.print(num + " ");
 			} else {
-				o.write(num + " ");
+				odd.print(num + " ");
 			}
 
 		}
 		sc.close();
-		o.close();
-		e.close();
+		odd.close();
+		even.close();
 		System.out.println("Even and Odd numbers separated successfully!");
 
 	}
