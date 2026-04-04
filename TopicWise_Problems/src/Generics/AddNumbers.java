d09f148c018e143550bfcb7b4fb8759bd6070fc9
Neyamul Adnan <muhammadmojiburrahaman@gmail.com>
2026-04-04 16:40:50 +0600

    initial

--- TopicWise_Problems/src/Generics/AddNumbers.java
+++ TopicWise_Problems/src/Generics/AddNumbers.java
@@ -1,16 +1,4 @@
 package Generics;
 
-public class AddNumbers<T extends Number> {
 
-	public T n1;
-	public T n2;
 
-	public AddNumbers(T n1, T n2) {
-		this.n1 = n1;
-		this.n2 = n2;
-	}
-
-	public double add() {
-		return n1.doubleValue() + n2.doubleValue();
-	}
-}
--- TopicWise_Problems/src/Generics/Main.java
+++ TopicWise_Problems/src/Generics/Main.java
@@ -3,6 +3,20 @@ package Generics;
 public class Main {
 
 	public static void main(String[] args) {
+		class AddNumbers<T extends Number> {
+
+			public T n1;
+			public T n2;
+
+			public AddNumbers(T n1, T n2) {
+				this.n1 = n1;
+				this.n2 = n2;
+			}
+
+			public double add() {
+				return n1.doubleValue() + n2.doubleValue();
+			}
+		}
 		// TODO Auto-generated method stub
 		AddNumbers<Integer> intNum = new AddNumbers<>(10, 20);
 		System.out.println("Integer Sum: " + intNum.add());
@@ -11,5 +25,5 @@ public class Main {
 		System.out.println("Double Sum: " + doubleNum.add());
 		
 	}
-
+	
 }
