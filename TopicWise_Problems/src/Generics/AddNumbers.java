f850ad4f3572c5586028d23cafe106ad4d2e3033
Neyamul Adnan <muhammadmojiburrahaman@gmail.com>
2026-04-04 16:50:29 +0600

    initial

--- TopicWise_Problems/src/Generics/AddNumbers.java
+++ TopicWise_Problems/src/Generics/AddNumbers.java
@@ -1,18 +1,22 @@
 package Generics;
 
+public class AddNumbers<T extends Number> {
+	public T n1;
+	public T n2;
 
-
-	public class AddNumbers<T extends Number>{
-		public T n1;
-		public T n2;
-		
-		AddNumbers(T n1, T n2){
-			this.n1 = n1;
-			this.n2 = n2;
-		}
-		public double add() {
-			return n1.doubleValue() + n2.doubleValue();
-		}
+	AddNumbers(T n1, T n2) {
+		this.n1 = n1;
+		this.n2 = n2;
 	}
 
+	public double add() {
+		return n1.doubleValue() + n2.doubleValue();
+	}
 
+	public static void main(String[] args) {
+
+		AddNumbers<Integer> in = new AddNumbers<>(5, 10);
+		
+
+	}
+}
