3acc4ca6e4ab4687edc96f8d151c0a2b70819b5f
Neyamul Adnan <muhammadmojiburrahaman@gmail.com>
2026-04-04 16:48:50 +0600

    initial

--- TopicWise_Problems/src/Generics/AddNumbers.java
+++ TopicWise_Problems/src/Generics/AddNumbers.java
@@ -2,3 +2,17 @@ package Generics;
 
 
 
+	public class AddNumbers<T extends Number>{
+		public T n1;
+		public T n2;
+		
+		AddNumbers(T n1, T n2){
+			this.n1 = n1;
+			this.n2 = n2;
+		}
+		public double add() {
+			return n1.doubleValue() + n2.doubleValue();
+		}
+	}
+
+
