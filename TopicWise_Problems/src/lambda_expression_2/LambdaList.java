8de126e169537440f16a6c102ee57ee10a788ca7
Neyamul Adnan <muhammadmojiburrahaman@gmail.com>
2026-04-16 00:06:47 +0600

    initial

--- TopicWise_Problems/src/lambda_expression_2/LambdaList.java
+++ TopicWise_Problems/src/lambda_expression_2/LambdaList.java
@@ -1,23 +1,34 @@
 package lambda_expression_2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LambdaList {
 	public static void main(String[] args) {
 
 		ArrayList<Integer> list = new ArrayList<>();
 		
-		list.add(1);
-		list.add(2);
-		list.add(3);
-		list.add(4);
-		list.add(5);
+		for(int i=1; i<=10; i++) {
+			list.add(i);
+		}
+		
 		
 		//Printing all the elements
 		list.forEach(n -> {
 			System.out.print(n + " ");
 		});
+		
+		//Sum of square of elements
 		int sum = list.stream().map(n -> n * n).reduce(0, Integer::sum);
 		System.out.println("\nSum of Squares = " + sum);
+		
+		// Odd and Even number separation
+		System.out.print("Even numbers:");
+        List<Integer> even = list.stream().filter(n -> n % 2 == 0).toList();
+        System.out.println(even);
+
+        System.out.print("Odd numbers:");
+        List<Integer> odd = list.stream().filter(n -> n % 2 != 0).toList();
+        System.out.println(odd);
 	}
 }
